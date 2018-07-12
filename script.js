822612e919232199e9c54b513d1e213d23aea3af
Abhinav Sharma <AbhiAdi@Abhinavs-MacBook-Pro.local>
2018-07-11 22:08:20 -0500

    final portfolio

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// Initialize Firebase
+
+  var config = {
+    apiKey: "AIzaSyAWjDL0-rTE_LRAkZ3lUiuqcB1sYTMz85U",
+    authDomain: "portfolio-abhinav.firebaseapp.com",
+    databaseURL: "https://portfolio-abhinav.firebaseio.com",
+    projectId: "portfolio-abhinav",
+    storageBucket: "",
+    messagingSenderId: "315855754982"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#submit").click(function (event) {
+    event.preventDefault();
+    name = $("#FormControlInput1").val().trim();
+    email = $("#FormControlInput2").val().trim();
+    message = $("#FormControlTextarea1").val().trim();
+    if(name.length>0 && email.length>0 && message.length>0){
+        database.ref("PortfolioMessages").push({
+            name: name,
+            email: email,
+            message: message
+        });
+    }
+    
+
+    $("#FormControlInput1").val("");
+    $("#FormControlInput2").val("");
+    $("#FormControlTextarea1").val("");
+});
